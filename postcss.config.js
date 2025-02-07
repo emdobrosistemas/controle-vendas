8d2dcf3f3a6f55da5fde9d744e8dbf0252240442
emdobrosistemas <emdobroapp@gmail.com>
2025-02-07 17:55:03 -0300

    Adiciona variáveis de ambiente e configurações do Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      overrideBrowserslist: ['> 1%', 'last 2 versions'],
+      add: true,
+      features: {
+        appearance: { properties: ['appearance', '-webkit-appearance', '-moz-appearance'] }
+      }
+    }
   }
-} 
+}
